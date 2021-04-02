16e2aea6e1fc7ea6173d8deca692652a35c65d98
anclaev <iahugo@yandex.ru>
2021-04-02 15:53:41 +0400

    Загрузка тестов с сервера.

--- src/containers/All/index.js
+++ src/containers/All/index.js
@@ -1,17 +1,43 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import axios from "axios";
 import classes from "./All.module.sass";
 
 export default class All extends Component {
+  state = {
+    quizes: [],
+  };
+
   renderQuizes = () => {
-    return [1, 2, 3].map((quiz, index) => {
+    return this.state.quizes.map((quiz) => {
       return (
-        <li className={classes.item} key={index}>
-          <NavLink to={`/quiz/${quiz}`}>Тест №{quiz}</NavLink>
+        <li className={classes.item} key={quiz.id}>
+          <NavLink to={`/quiz/${quiz.id}`}>{quiz.name}</NavLink>
         </li>
       );
     });
   };
+
+  async componentDidMount() {
+    try {
+      const response = await axios.get(
+        "https://ancla-assay-default-rtdb.firebaseio.com/quizes.json"
+      );
+
+      const quizes = [];
+
+      Object.keys(response.data).forEach((key, index) => {
+        quizes.push({
+          id: key,
+          name: `Тест №${index + 1}`,
+        });
+
+        this.setState({ quizes });
+      });
+    } catch (e) {
+      console.log("e: ", e);
+    }
+  }
 
   render() {
     return (
